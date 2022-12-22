c72d367e717b5c0cfbe5b06a9ee46348c43aadcd
deepakmahapatra047 <107745111+deepakmahapatra047@users.noreply.github.com>
2022-12-22 23:00:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,29 @@ import React from "react";
 import "../styles/App.css";
 import Component from "./Component";
 import ErrorBoundary from "./ErrorBoundary";
-const App = () => {
 
+const App = () => {
+  let setFunc;
+
+  let changeFunc = (func) => {
+    setFunc = func;
+  };
+
+  const errorGenerator = () => {
+    setFunc(true);
+  };
 
   return (
-
     <div id="main">
-    <h2>React Error Boundaries</h2>
+      <h2>React Error Boundaries</h2>
+
       <ErrorBoundary>
         <Component change={changeFunc} />
       </ErrorBoundary>
-      <button id="gen" onClick={errorGenerator}>Generate Error</button>
+
+      <button id="gen" onClick={errorGenerator}>
+        Generate Error
+      </button>
     </div>
   );
 };
